665fdac11aa147e7e870cd0366a3515e69e3e272
J <jonathan.arnould33@gmail.com>
2020-10-24 13:42:03 +0200

    feature-help modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Help from './components/help/Help';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/Help" component={Help} />
       </Switch>
     </div>
   );
---
+++ src/components/help/Help.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Help() {
+  return (
+    <div className="help-modal">
+      <h1>Mysterium Rules</h1>
+      <div>Help Me!</div>
+    </div>
+  );
+}
+export default Help;
