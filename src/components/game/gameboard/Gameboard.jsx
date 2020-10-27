52ba3c4adbe7e8be6c9651d1b98a8f289c08ffe4
J <jonathan.arnould33@gmail.com>
2020-10-27 16:26:28 +0100

    feature/ API call

---
+++ src/components/game/gameboard/Gameboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GameTable from './gametable/GameTable';
+
+const Gameboard = () => {
+  return <GameTable />;
+};
+
+export default Gameboard;
--- src/components/help/Help.jsx
+++ src/components/help/Help.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Help() {
+import '../../styles/css/Help.css';
+
+function Help(props) {
+  const { show } = props;
+  if (!show) {
+    return null;
+  }
   return (
     <div className="help-modal">
       <h1>Mysterium Rules</h1>
@@ -8,4 +15,8 @@ function Help() {
     </div>
   );
 }
+
+Help.propTypes = {
+  show: PropTypes.bool.isRequired,
+};
 export default Help;
