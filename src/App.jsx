9b5e8b71e95ec4111e9f39816a9e5a5a335db19a
FlorianPx <florian.poux@hotmail.fr>
2020-10-28 11:02:01 +0100

    Modify css and add router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Medium from './components/medium/Medium';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/medium" component={Medium} />
       </Switch>
     </div>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,19 +2,38 @@ import React, { Component } from 'react';
 import '../../styles/css/Home.css';
 import titleLogo from '../../styles/images/titleLogo.png';
 
-function Home() {
-  return (
-    <div>
-      <div className="titleHome">
-        <img className="titleStyle" src={titleLogo} alt="Game Title" />
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.submitHome = this.submitHome.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange() {
+    this.setState();
+  }
+  submitHome() {}
+
+  render() {
+    return (
+      <div>
+        <div className="titleHome">
+          <img className="titleStyle" src={titleLogo} alt="Game Title" />
+        </div>
+        <form className="inputHome" onSubmit={this.submitHome}>
+          <input
+            placeholder="Entrez votre pseudo..."
+            className="inputStyle"
+            onChange={this.onChange}
+          ></input>
+        </form>
+        <div className="buttonHome">
+          <button type="submit" className="buttonStyle">
+            Commencer
+          </button>
+        </div>
       </div>
-      <div className="inputHome">
-        <input className="inputStyle"></input>
-      </div>
-      <div className="buttonHome">
-        <button className="buttonStyle">Commencer</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 export default Home;
