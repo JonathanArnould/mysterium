af5bb9d4c259dd62a21c17669ecdb018570f2565
surlejonathan <surle.jonathan@gmail.com>
2020-10-29 00:44:31 +0100

    timer

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import '../../styles/css/Navbar.css';
 import crystalBall from '../../styles/images/image-678070.png';
 import clock from '../../styles/images/clock.resized.png';
+import home from '../../styles/images/home.png';
 
 const Navbar = () => {
   const [player, setPlayer] = useState('');
@@ -14,26 +15,44 @@ const Navbar = () => {
       .then((responses) => setPlayer(responses.data.results[0]));
   }, []);
 
+  const [timeLeft, setTimeLeft] = useState(60);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [timeLeft]);
+
   return (
     <nav>
-      {player ? (
-        <img
-          className="avatar"
-          src={player.picture.thumbnail}
-          alt={player.name.first}
-        />
-      ) : (
-        <p>Loading</p>
-      )}
-      <span>Timer</span>
+      <div className="nav-left">
+        {player ? (
+          <img
+            className="nav-item avatar"
+            src={player.picture.thumbnail}
+            alt={player.name.first}
+          />
+        ) : (
+          <p>Loading</p>
+        )}
+        <span className="nav-item counter">
+          {!timeLeft ? 'Temps écoulé!' : `: ${timeLeft}`}
+        </span>
+      </div>
+
       <img className="clock" src={clock} alt="clock" />
-      <Link to="/">Home</Link>
-      <Link to="/help">
-        <div className="crystalBall-container">
-          <img id="crystalBall" src={crystalBall} alt="crystal ball" />
-          <span className="interroMark">?</span>
-        </div>
-      </Link>
+      <div className="nav-right">
+        <Link to="/">
+          <img className="nav-item icon-home" src={home} alt="home" />
+        </Link>
+        <Link to="/help">
+          <div className="nav-item crystalBall-container">
+            <img id="crystalBall" src={crystalBall} alt="crystal ball" />
+            <span className="interroMark">?</span>
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 };
