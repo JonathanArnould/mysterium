e71f6a6498efd2442681b0164df323ab0296ef04
Paus <mariemeline.paus@gmail.com>
2020-11-02 11:25:19 +0100

    add jon router rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/home/Home';
-import Help from './components/help/Help';
 import Gameboard from './components/gameboard/GameBoard';
 import Win from './components/end/Win';
 import Medium from './components/medium/Medium';
 import Lose from './components/end/Lose';
 import End from './components/end/End';
+import Help from './components/help/Help';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false,
-    };
-  }
-
-  showHelp() {
-    const { show } = this.state;
-    this.setState({
-      show: !show,
-    });
-  }
-
-  render() {
-    const { show } = this.state;
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/medium" component={Medium} />
-          <Route path="/board" component={Gameboard} />
-          <Route path="/win" component={Win} />
-          <Route path="/lose" component={Lose} />
-          <Route path="/end" component={End} />
-        </Switch>
-        <button
-          type="submit"
-          className="help-btn"
-          onClick={() => {
-            this.showHelp();
-          }}
-        >
-          Help Me!
-        </button>
-        <Help show={show} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/medium" component={Medium} />
+        <Route path="/board" component={Gameboard} />
+        <Route path="/win" component={Win} />
+        <Route path="/lose" component={Lose} />
+        <Route path="/end" component={End} />
+        <Route path="/help" component={Help} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
