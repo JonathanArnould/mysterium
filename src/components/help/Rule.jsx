4ce9d0e3e6de4a95626ca442ceed7f649a25b0bd
surlejonathan <surle.jonathan@gmail.com>
2020-11-02 15:01:15 +0100

    refactoring: put /navbar into /gameboard - add classname title-modal,subtitle-modal,image-modal,paragraphe-modal to Rule.jsx/css

--- src/components/help/Rule.jsx
+++ src/components/help/Rule.jsx
@@ -12,22 +12,30 @@ const Rule = () => {
   };
   return (
     <div className="rule-container">
-      <h2> Règles de jeu</h2>
+      <h2 className="title-modal"> Règles de jeu</h2>
       {/* eslint-disable react/jsx-props-no-spreading */}
       <Slider {...settings}>
         <div>
-          <h3>Bienvenue dans Mysterium!</h3>
-          <img src="https://via.placeholder.com/800x450" alt="screenshots" />
-          <p>
+          <h3 className="subtitle-modal">Bienvenue dans Mysterium!</h3>
+          <img
+            className="image-modal"
+            src="https://via.placeholder.com/800x450"
+            alt="screenshots"
+          />
+          <p className="paragraphe-modal">
             Je suis l&apos;esprit qui hante ces lieux, aidez moi à retrouver mon
             assassin. Je ne me rappelle pas précisément du coupable mais
             j&apos;ai quelques visions...
           </p>
         </div>
         <div>
-          <h3>Cartes Visions</h3>
-          <img src="https://via.placeholder.com/800x450" alt="screenshots" />
-          <p>
+          <h3 className="subtitle-modal">Cartes Visions</h3>
+          <img
+            className="image-modal"
+            src="https://via.placeholder.com/800x450"
+            alt="screenshots"
+          />
+          <p className="paragraphe-modal">
             Je vais tout d&apos;abord vous transmettre une vision qui va se
             matérialiser par une carte.
             <br /> En premier lieu il s&apos;agira de retrouver l&apos;arme du
@@ -35,9 +43,13 @@ const Rule = () => {
           </p>
         </div>
         <div>
-          <h3>Carte armes, lieux et suspects</h3>
-          <img src="https://via.placeholder.com/800x450" alt="screenshots" />
-          <p>
+          <h3 className="subtitle-modal">Carte armes, lieux et suspects</h3>
+          <img
+            className="image-modal"
+            src="https://via.placeholder.com/800x450"
+            alt="screenshots"
+          />
+          <p className="paragraphe-modal">
             Vous devrez ensuite comparer la ou les cartes visions précédemment
             obtenues afin d&apos;identifier l&apos;arme, puis le lieu et enfin
             le coupable. chaque fois que vous trouvez la bonne réponse, vous
@@ -46,9 +58,13 @@ const Rule = () => {
           </p>
         </div>
         <div>
-          <h3>Temps imparti</h3>
-          <img src="https://via.placeholder.com/800x450" alt="screenshots" />
-          <p>
+          <h3 className="subtitle-modal">Temps imparti</h3>
+          <img
+            className="image-modal"
+            src="https://via.placeholder.com/800x450"
+            alt="screenshots"
+          />
+          <p className="paragraphe-modal">
             Vous avez 1 minute pour faire votre choix à chaque fois qu&apos;une
             carte vision est distribuée. Si vous n&apos;avez pas fait de choix à
             la fin du temps imparti, vous ne trouvez pas la bonne réponse et une
@@ -56,9 +72,13 @@ const Rule = () => {
           </p>
         </div>
         <div>
-          <h3>Fin de la partie</h3>
-          <img src="https://via.placeholder.com/800x450" alt="screenshots" />
-          <p>
+          <h3 className="subtitle-modal">Fin de la partie</h3>
+          <img
+            className="image-modal"
+            src="https://via.placeholder.com/800x450"
+            alt="screenshots"
+          />
+          <p className="paragraphe-modal">
             Lorsque vous aurez réunis toute les preuves et trouvé le coupable
             vous apaiserez mes tourments et je pourrais alors cesser de hanter
             ces lieux. Si l&apos;on ne parvient pas à identifier mon tueur je
