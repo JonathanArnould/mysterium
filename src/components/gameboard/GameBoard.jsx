2982976a0be87a456310319b77c60c73c1327240
J <jonathan.arnould33@gmail.com>
2020-11-03 15:09:47 +0100

    feature-aide de jeu-integration à la Navbar

--- src/components/gameboard/GameBoard.jsx
+++ src/components/gameboard/GameBoard.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import GameTable from './gametable/GameTable';
 import Navbar from '../navbar/Navbar';
 import '../../styles/css/component/GameBoard.css';
-import Rule from '../help/Rule';
 
 const GameBoard = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -12,8 +11,7 @@ const GameBoard = () => {
   return (
     <div className="GameBoard">
       <Navbar setModalIsOpen={handleSetModalIsOpen} modalIsOpen={modalIsOpen} />
-      <GameTable />
-      {modalIsOpen && <Rule />}
+      <GameTable modalIsOpen={modalIsOpen} />
     </div>
   );
 };
