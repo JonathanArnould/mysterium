e7cbd1916383821ed21ffd59d988b84f9416eaf2
J <jonathan.arnould33@gmail.com>
2020-11-05 11:31:07 +0100

    feature- générer arme-lieu-murder en random

--- src/components/gameboard/GameBoard.jsx
+++ src/components/gameboard/GameBoard.jsx
@@ -1,17 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import axios from 'axios';
 import GameTable from './gametable/GameTable';
 import Navbar from './navbar/Navbar';
 import '../../styles/css/component/GameBoard.css';
 
+export const charWeaponPlace = {
+  weapon: '',
+  place: '',
+  murder: '',
+};
+
+export const CharWeaponPlaceContext = createContext(charWeaponPlace);
+
 const GameBoard = () => {
+  useEffect(() => {
+    axios('https://mysterium-game.herokuapp.com/api/characters')
+      .then((response) => response.data)
+      .then((data) => {
+        const random = Math.floor(Math.random() * data.length);
+        localStorage.setItem('murder', JSON.stringify(data[random]));
+        // setCharWeaponPlace({ ...charWeaponPlace, murder: data[random] });
+      });
+    axios('https://mysterium-game.herokuapp.com/api/weapons')
+      .then((response) => response.data)
+      .then((data) => {
+        const random = Math.floor(Math.random() * data.length);
+        localStorage.setItem('weapon', JSON.stringify(data[random]));
+        // setCharWeaponPlace({ ...charWeaponPlace, weapon: data[random] });
+      });
+    axios('https://mysterium-game.herokuapp.com/api/places')
+      .then((response) => response.data)
+      .then((data) => {
+        const random = Math.floor(Math.random() * data.length);
+        localStorage.setItem('place', JSON.stringify(data[random]));
+        // setCharWeaponPlace({ ...charWeaponPlace, place: data[random] });
+      });
+  }, []);
+
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const handleSetModalIsOpen = () => {
     setModalIsOpen(!modalIsOpen);
   };
+
   return (
     <div className="GameBoard">
-      <Navbar setModalIsOpen={handleSetModalIsOpen} modalIsOpen={modalIsOpen} />
-      <GameTable modalIsOpen={modalIsOpen} />
+      <CharWeaponPlaceContext.Provider value={charWeaponPlace}>
+        <Navbar
+          setModalIsOpen={handleSetModalIsOpen}
+          modalIsOpen={modalIsOpen}
+        />
+        <GameTable modalIsOpen={modalIsOpen} />
+      </CharWeaponPlaceContext.Provider>
     </div>
   );
 };
--- src/components/gameboard/gametable/GameTable.jsx
+++ src/components/gameboard/gametable/GameTable.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import GameBody from './gamebody/GameBody';
 import '../../../styles/css/component/GameTable.css';
 import Rule from '../../help/Rule';
+import { CharWeaponPlaceContext } from '../GameBoard';
 
 const GameTable = ({ modalIsOpen }) => {
+  const murder = useContext(CharWeaponPlaceContext);
+
+  console.log(murder);
   return (
     <div className="GameTable">
       <GameBody /> {modalIsOpen && <Rule />}
