97494fce5a00e24f4381d1aca20a8233a17caacd
surlejonathan <surle.jonathan@gmail.com>
2020-11-11 19:30:28 +0100

    GameBoard usecontext

--- src/components/gameboard/gametable/gamebody/GameBody.jsx
+++ src/components/gameboard/gametable/gamebody/GameBody.jsx
@@ -5,24 +5,72 @@ import ZoomCard from './ZoomCard';
 import '../../../../styles/css/component/GameBody.css';
 
 const GameBody = () => {
-  const [cards, setCards] = useState([]);
+  const [leftCards, setLeftCards] = useState({ items: [], activeItem: null });
+  const [step] = useState({
+    step1: true,
+    step2: false,
+    step3: false,
+  });
+  const [zoomCards, setZoomCards] = useState('');
 
   useEffect(() => {
     Axios('https://mysterium-game.herokuapp.com/api/weapons')
       .then((responses) => responses.data)
-      .then((data) => setCards(data));
+      .then((data) => {
+        localStorage.setItem('weapons', JSON.stringify(data));
+        const weapons = localStorage.getItem('weapons');
+        return (
+          step.step1 &&
+          setLeftCards({ ...leftCards, items: JSON.parse(weapons) })
+        );
+      });
+
+    Axios('https://mysterium-game.herokuapp.com/api/places')
+      .then((responses) => responses.data)
+      .then((data) => {
+        localStorage.setItem('places', JSON.stringify(data));
+        const places = localStorage.getItem('places');
+        return (
+          step.step2 &&
+          setLeftCards({ ...leftCards, items: JSON.parse(places) })
+        );
+      });
+
+    Axios('https://mysterium-game.herokuapp.com/api/characters')
+      .then((responses) => responses.data)
+      .then((data) => {
+        localStorage.setItem('characters', JSON.stringify(data));
+        const characters = localStorage.getItem('characters');
+        return (
+          step.step3 &&
+          setLeftCards({ ...leftCards, items: JSON.parse(characters) })
+        );
+      });
   }, []);
 
-  const createStockcard = cards.map((card) => (
+  const createStockcard = leftCards.items.map((card) => (
     <div key={card.id} className="stockcard-card">
-      <img className="stockcard-image" src={card.image} alt={card.name} />
+      <img
+        onClick={() => {
+          setZoomCards(card);
+          setLeftCards({ ...leftCards, activeItem: card.name });
+        }}
+        aria-hidden="true"
+        className={`stockcard-image ${
+          leftCards.activeItem === card.name ? 'active' : ''
+        }`}
+        src={card.image}
+        alt={card.name}
+      />
     </div>
   ));
+
+  const createZoomCardLeft = <img src={zoomCards.image} alt={zoomCards.name} />;
 
   return (
     <div className="GameBody">
       <StockCard className="stockcardleft" content={createStockcard} />
-      <ZoomCard className="zoomcardleft" />
+      <ZoomCard className="zoomcardleft" content={createZoomCardLeft} />
       <ZoomCard className="zoomcardright" />
       <StockCard className="stockcardright" />
     </div>
